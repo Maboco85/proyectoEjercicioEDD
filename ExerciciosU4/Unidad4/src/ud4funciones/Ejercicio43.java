26ced0a4b221a8fdb5fb3a477447a4d6669f52e1
MarioBC <mariopolinya@hotmail.com>
2024-11-22 19:23:06 +0100

    No se permiten números negativos y se elimina Ejercicio41.java

--- ExerciciosU4/Unidad4/src/ud4funciones/Ejercicio43.java
+++ ExerciciosU4/Unidad4/src/ud4funciones/Ejercicio43.java
@@ -2,6 +2,13 @@ package ud4funciones;
 
 public class Ejercicio43 {
 	static void dibujarRectangulo(int ancho, int alto) {
+		
+		// Modificación--> error si ancho o alto no son positivos
+		if (ancho <= 0 || alto <= 0) { 
+			System.out.println("Error: El ancho y el alto del rectángulo deben ser positivos."); 
+			return; 
+		}
+		
 
 		for (int i = 0; i < alto; i++) {
 			for (int j = 0; j < ancho; j++) {
